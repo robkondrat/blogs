cb638d5e3283b0ca52fe8a0cdaddd8874a396df9
EatYourSoup7 <r.kondratowicz1@gmail.com>
2021-01-21 11:31:31 -0600

    refactor UserHeader

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -7,9 +7,8 @@ class UserHeader extends React.Component {
     this.props.fetchUser(this.props.userId);
   }
   
-  render() {
-
-    const user = this.props.users.find((user) => user.id === this.props.userId);
+  render() {  
+    const { user } = this.props;
     
     if (!user) {
       return null;
@@ -19,8 +18,8 @@ class UserHeader extends React.Component {
   }
 }
 
-const mapStateToProps = (state) => {
-  return { users: state.users };
+const mapStateToProps = (state, ownProps) => {
+  return { user: state.users.find(user => user.id === ownProps.userId) };
 };
 
 export default connect(mapStateToProps, { fetchUser })(UserHeader);
